234a34e71af8893f6715a55cafc98e26383414aa
kangsubeen <sbin3837@gmail.com>
2019-07-03 01:10:47 +0900

    Updates

---
+++ precache-manifest.711c5a138921f7c9d589768d9136ef10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d292bf856280c96a7d19748a103915",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "ec2a29f603a659a611a4",
+    "url": "/movie_app/static/css/main.2a0f928d.chunk.css"
+  },
+  {
+    "revision": "5cdb4062c6dfdd077298",
+    "url": "/movie_app/static/js/2.31ebf22a.chunk.js"
+  },
+  {
+    "revision": "ec2a29f603a659a611a4",
+    "url": "/movie_app/static/js/main.16e6ac1f.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
